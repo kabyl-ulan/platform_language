192f1f9687103635bc2f6743b61546ff9509f678
kabyl-ulan <kabylulanabdykaykuulu@gmail.com>
2023-04-23 17:45:56 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <>Ulan</>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+//local
+import "./index.scss";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>
+);
