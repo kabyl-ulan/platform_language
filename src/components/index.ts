5125d196c77c11c58e124d0fc768eb152523c889
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2023-06-02 12:20:44 +0600

    add hero home page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Header } from "./header/Header";
 export { default as Footer } from "./footer/Footer";
+
+//components Home page
+export { default as HeroHome } from "./components-home/HeroHome";
