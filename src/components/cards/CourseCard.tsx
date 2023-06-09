9e6864831c1d84202499f5534469dcc4f75342d0
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2023-06-09 23:54:15 +0600

    change course card

--- src/components/cards/CourseCard.tsx
+++ src/components/cards/CourseCard.tsx
@@ -18,32 +18,43 @@ const CourseCard: FC = () => {
   return (
     <Box
       border="1px"
-      mb="10px"
+      mb="15px"
       display="flex"
-      flexDir={{ base: "column", sm: "row" }}
+      flexDir={{ base: "column", xs: "row" }}
       onClick={onClick}
     >
-      <Box overflow="hidden">
+      <Box overflow="hidden" w={{ base: "100%", xs: "350px" }}>
         <Images
           source={CourseImage}
           alt="course_image"
-          maxWidth={{ base: "100%", sm: "350px" }}
+          maxWidth="100%"
           _hover={{ transform: "scale(1.1)" }}
           cursor="pointer"
         />
       </Box>
-      <Box p="5px" display="flex" flexDir="column">
-        <Text fontSize="26px">Полный курс по JavaScript</Text>
-        <Text mt="10px">
+      <Box p="8px" display="flex" flexDir="column" w="100%">
+        <Text
+          fontSize={{ base: "24px", md: "26px" }}
+          fontWeight="800"
+          lineHeight="120%"
+        >
+          Полный курс по JavaScript
+        </Text>
+        <Text mt="10px" textAlign="justify">
           Полныи курс по JavaScript С нуля до результата Это ранняя версия
           курса. Курс готов на 70% и охватывает все основные темы JS.
         </Text>
-        <Box display="flex" alignItems="center" justifyContent="space-around">
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-around"
+          flexWrap="wrap"
+        >
           <Box display="flex" alignItems="center">
             <MdLanguage />
             <Text ml="5px">Русский</Text>
           </Box>
-          <Box display="flex" alignItems="center">
+          <Box display="flex" alignItems="center" mx="10px">
             <GoClock />
             <Text ml="5px">05:39:50</Text>
           </Box>
